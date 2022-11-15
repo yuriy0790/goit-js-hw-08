26f858b68535594efc2ce8587abe0191c82e3310
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-11-15 09:25:05 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,15 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const throttle = require('lodash.throttle');
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function getTime(event) {
+  localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+}
+
+player.on('timeupdate', throttle(getTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
+
+// player.off('timeupdate');
