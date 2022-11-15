680d29ff944c64b9a101129e5cae3842adc81c9e
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-11-15 11:29:49 +0200

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,14 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 const throttle = require('lodash.throttle');
 
 function getTime(event) {
-  localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+  localStorage.setItem('videoplayer-current-time', event.seconds);
 }
 
 player.on('timeupdate', throttle(getTime, 1000));
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
 // player.off('timeupdate');
